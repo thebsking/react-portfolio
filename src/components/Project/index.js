065396acca3b6fd193b078a11c8db8ce6bbdc9e9
Brandon King <brandonscottking@gmail.com>
2021-06-13 22:41:18 -0400

    refactoring project.js

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import Card from '../Card';
 
-function Project() {
-    const projectList = [
-        {
-            title: 'The Loo Review',
-            image: 'add a link here',
-            description: 'find yo self a tur-let'
-        },
-        {
-            title: 'Weather Search',
-            image: '/public/bk-headshot.png',
-            description: '*not an actual project image'
-        },
-        {
-            title: 'another PJ',
-            image: '/bk-headshot.png',
-            description: "i'm very bad at writing these things especially in a testing environment where they don't really mean anything"
-        }
-    ]
+function Project(props) {
+    
     return(
         <div id="project-container">
-            {projectList.map(proj => {
+            {props.map(proj => {
                 return <Card {...proj} />
             })}
             {console.log('i made a card!')}
