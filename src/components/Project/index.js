1f875d7dcbcb2054de81ee692ab22388e93edd42
Brandon King <brandonscottking@gmail.com>
2021-06-13 21:51:32 -0400

    working on home page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -12,6 +12,11 @@ function Project() {
             title: 'Weather Search',
             image: '/public/bk-headshot.png',
             description: '*not an actual project image'
+        },
+        {
+            title: 'another PJ',
+            image: '/bk-headshot.png',
+            description: "i'm very bad at writing these things especially in a testing environment where they don't really mean anything"
         }
     ]
     return(
