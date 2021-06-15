ad6cc581feac7dce16e91459557b43bfe6d93a23
Brandon King <brandonscottking@gmail.com>
2021-06-14 22:35:54 -0400

    added logos to skills section

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,47 +4,49 @@ function Home() {
     const skillsArr = [
         {
             desc: 'HTML5',
-            icon: ''
+            icon: <i className="devicon-html5-plain colored" />
         },
         {
             desc: 'CSS3',
-            icon: ''
+            icon: <i className="devicon-css3-plain colored" />
         },
         {
             desc: 'JavaScript',
-            icon: ''
+            icon: <i className="devicon-javascript-plain colored" />
         },
         {
             desc: 'jQuery',
-            icon: ''
+            icon: <i className="devicon-jquery-plain colored" />
         },{
             desc: 'NodeJs',
-            icon: ''
+            icon: <i className="devicon-nodejs-plain colored" />
+        },
+        {
+            desc: 'git',
+            icon: <i className="devicon-git-plain colored" />
+        },
+        {
+            desc: 'handlebars',
+            icon: <i className="devicon-handlebars-plain colored" />
         },
         {
             desc: 'React',
-            icon: ''
-        },
-        {
-            desc: 'MongoDb',
-            icon: ''
-        },
-        {
-            desc: 'MySql',
-            icon: ''
+            icon: <i className="devicon-react-plain colored" />
         }
     ]
     return(
         <main>
             <section id="welcome">
                 <h3>about me</h3>
-                <p>hi, i am brandon. i am a full-stack web developer. i am currently available for freelance work. you can view some of my recent projects <a href='/gallery'>here</a>. if you would like to work with me you can contact me <a href='/contact'>here</a>.</p>
+                <p>hi, i am brandon. i am a full-stack web developer located in columbus,oh. i am a musician that loves facts and figures. that blend of analytical and creative thinking is what drew me to coding.</p>
+                <p>i am currently available for freelance work. you can view some of my recent projects <a href='/gallery'>here</a>. if you would like to work with me you can contact me <a href='/contact'>here</a>.</p>
             </section>
             <section id="technologies">
                 <h3>skills</h3>
                 {skillsArr.map(skill => {
                     return (
-                        <img src={skill.icon} alt={`${skill.desc} icon`} />
+                        <span>{skill.icon}
+                       {skill.desc}</span>
                     )
                 })}
             </section>
