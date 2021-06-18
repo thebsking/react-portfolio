f2b21b5c39de82b8bb562fd7ac681884673e4ba4
Brandon King <brandonscottking@gmail.com>
2021-06-18 10:18:00 -0400

    added images for cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,30 +7,30 @@ function Project() {
     const projectArr = [
         {
             title: 'Loo Review',
-            img: '/logo192.png',
+            img: '/assets/loo-review.gif',
             link: '/Project-02',
             description: 'Think of it as Yelp for bathrooms. This web app utilizes your location to show you the nearest public restrooms, complete with user reviews. '
         },
         {
             title: 'Weather Search',
-            img: '/logo192.png',
+            img: '/assets/weather-search.gif',
             link: '/weather-search',
             description: 'A simple web app utilizing the OpenWeather API that allows you to search current and future forecast data. '
         },
         {
             title: 'Budget App',
-            img: '/logo192.png',
+            img: '/assets/screenshot.png',
             link: '/pwa-budget',
             description: 'A Progressive Web App that allows you to track credits and debits. Functionality continues when offline.'
         }, {
             title: 'Tech Blog',
-            img: '/logo192.png',
+            img: '/assets/preview.gif',
             link: 'tech-blog',
             description: 'A CMS style blogging site. Users can login to see their posts and comment on posts from others.'
         },
         {
             title: 'Employee Directory',
-            img: '/logo192.png',
+            img: '/assets/sorted.png',
             link: '/react-directory',
             description: 'Built with react, a simple interface allows you to see all of your companies employees in an easy to sort list.'
         },
