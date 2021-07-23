e0438fd0081e1160b70490530d93a58e4e207949
Brandon King <brandonscottking@gmail.com>
2021-07-22 21:35:34 -0400

    added base code for nodejs server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
