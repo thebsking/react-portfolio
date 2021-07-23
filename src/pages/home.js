aa0e7b5146aabffb9b04d20ee878af4e3db98029
Brandon King <brandonscottking@gmail.com>
2021-07-22 21:24:29 -0400

    refactored home.js to remove console errors

--- src/pages/home.js
+++ src/pages/home.js
@@ -36,7 +36,7 @@ function Home() {
         },
         {
             desc: 'WordPress',
-            icon: <i class="devicon-wordpress-plain"></i>
+            icon: <i className="devicon-wordpress-plain"></i>
         }
     ]
     return (
@@ -55,11 +55,11 @@ function Home() {
                 <div className='skill-container'>
                     {skillsArr.map(skill => {
                         return (
-                            <div>
-                            <span className='skill-desc'>{skill.desc}</span>
-                            <br />
-                            <span className='skill-logo'>{skill.icon}</span>
-                            
+                            <div key={skill.desc}>
+                                <span className='skill-desc'>{skill.desc}</span>
+                                <br />
+                                <span className='skill-logo'>{skill.icon}</span>
+
                             </div>
                         )
                     })}
@@ -85,14 +85,14 @@ function Home() {
                     <p>big brothers big sisters of central ohio | 2017-2021</p>
                     <Button id="res-btn" color="red" href="/assets/bk-resume.pdf">Download Resume</Button>
                 </div>
-                
+
             </section>
             <section id="social">
                 <h3 className="section-head">social</h3>
                 <div className='social-container'>
-                    <a class='no-deco social-link' href="https://github.com/thebsking"><Icon color='red' size='huge' name='github' /></a>
-                    <a class='no-deco social-link' href="https://linkedin.com/in/brandonsking"><Icon color="red" name='linkedin' size='huge' /></a>
-                    <a class='no-deco social-link' href="https://twitter.com/thebsking"><Icon name='twitter' size='huge' color="red" /></a>
+                    <a className='no-deco social-link' href="https://github.com/thebsking"><Icon color='red' size='huge' name='github' /></a>
+                    <a className='no-deco social-link' href="https://linkedin.com/in/brandonsking"><Icon color="red" name='linkedin' size='huge' /></a>
+                    <a className='no-deco social-link' href="https://twitter.com/thebsking"><Icon name='twitter' size='huge' color="red" /></a>
                 </div>
             </section>
         </main>
